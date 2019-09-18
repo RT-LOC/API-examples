9ba1a54cfd5825cc7b05f896b578b54015ed4462
Wim <wimmar0@gmail.com>
2019-09-18 14:53:55 +0200

    MQTT client example

---
+++ Data API/json/README.md
@@ -0,0 +1,13 @@
+# MQTT client demo
+
+This demo code connects to the RTLOC MQTT broker (using MQTT.js), subscribes to a few topics, and prints the output. (using a demo user, and always-on demo MQTT data)
+The code can be used in Node as well as in the browser (using e.g. webpack).
+
+## Getting started
+
+* (No setup prerequisites, this demo will connect with our broker)
+* Run `npm run start`
+
+## Dependencies
+
+* MQTT.js (https://www.npmjs.com/package/mqtt)
---
+++ Data API/json/mqttClient.js
@@ -0,0 +1,29 @@
+const mqtt = require('mqtt')
+
+mqttOptions = {
+  keepalive: 50,
+  clientId: 'client_' + Math.random().toString(16).substr(2, 8),
+  protocolId: 'MQTT',
+  protocolVersion: 4,
+  clean: true,
+  reconnectPeriod: 60000,
+  connectTimeout: 30 * 1000,
+  username: 'demo:demo@rtloc.com',
+  password: '12345', // Alternative: access token instead of password
+  rejectUnauthorized: false
+}
+
+// Connect to MQTT broker
+const client = mqtt.connect('wss://mqtt.cloud.rtloc.com:8083/ws', mqttOptions)
+
+// Subscribe to a few topics
+client.subscribe('rtls/kart/status', { qos: 0 })
+client.subscribe('rtls/kart/anchors', { qos: 0 })
+client.subscribe('rtls/kart/posxyz', { qos: 0 })
+
+// On message: print topic and JSON message
+client.on('message', function (topic, message) {
+  const json = JSON.parse(message)
+  console.log(topic + ' message:')
+  console.log(json)
+})
