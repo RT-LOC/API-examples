3956f462245607282012b1a827dbb3e3666f72dd
bepnos <matarostafke.langeraert@student.kuleuven.be>
2023-07-16 14:08:44 +0200

    [EXAMPLES][PYTHON] - bugfix, refactoring

--- examples/python/distances/gui.py
+++ examples/python/distances/gui.py
@@ -3,7 +3,7 @@ import asyncio
 import sys
 from pathlib import Path
 from PySide6.QtWidgets import (QApplication, QMainWindow, QPushButton,
-                               QTextEdit, QVBoxLayout, QWidget, QHBoxLayout, QSizePolicy, QLabel, QLineEdit, QComboBox)
+                               QTextEdit, QVBoxLayout, QWidget, QHBoxLayout, QSizePolicy, QLabel, QLineEdit, QComboBox, QStackedWidget, QTableWidget)
 from PySide6.QtGui import QIcon, QTextCursor
 from qasync import QEventLoop
 
@@ -17,45 +17,76 @@ import parsers.socket.Python.udp_client
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.setWindowTitle("RTLOC API Tools - Distance Example")
-        self.setWindowIcon(QIcon("RTLOCpng.png"))
-        self.resize(800, 600)
-        # Setup UI elements
-        self.setup_ui()
-        
-        # Initialize booleans to avoid exceptions
+        self.init_ui()
+        self.init_connection_state()
+
+    def init_ui(self):
+        """Initializes all UI components and configures UI layout."""
+        self.configure_window()
+        self.create_display_stack()
+        self.create_control_buttons()
+        self.create_config_widgets()
+        self.configure_layouts()
+
+    def init_connection_state(self):
+        """Initializes state variables for connection and script running status."""
         self.running = False
         self.connected = False
 
-    def setup_ui(self): 
-        """Setup UI elements."""
-        self.text_edit = QTextEdit()
-        self.text_edit.setReadOnly(True)
-        self.text_edit.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-        
-        # Control Widgets
+    def configure_window(self):
+        """Configures the main window properties."""
+        self.setWindowTitle("RTLOC API Tools - Distance Example")
+        self.setWindowIcon(QIcon("RTLOCpng.png"))
+        self.resize(800, 600)
+
+    def create_display_stack(self):
+        """Creates the display stack for switching between text and table display."""
+        self.display_stack = QStackedWidget()
+        self.text_display = QTextEdit()
+        self.text_display.setReadOnly(True)
+        self.table_display = QTableWidget()
+        self.display_stack.addWidget(self.text_display)
+        self.display_stack.addWidget(self.table_display)
+        self.display_stack.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+
+    def create_control_buttons(self):
+        """Creates buttons for controlling the script."""
         self.button_start = QPushButton("Start")
         self.button_start.clicked.connect(self.start)
-        
+
         self.button_pause = QPushButton("Pause")
         self.button_pause.clicked.connect(self.pause)
-        
+
         self.button_stop = QPushButton("Stop")
         self.button_stop.clicked.connect(self.stop)
-        
-        # Config widgets
+
+    def create_config_widgets(self):
+        """Creates widgets for configuring the script."""
         self.ip_edit = QLineEdit()
         self.ip_edit.setPlaceholderText("Enter IP address")
-        
+
         self.port_edit = QLineEdit()
         self.port_edit.setPlaceholderText("Enter port number")
-        
+
         self.config_dropdown = QComboBox()
         self.config_dropdown.setPlaceholderText("Select config")
         self.config_dropdown.addItems(["main.py", "cross.py"])
         self.config_dropdown.currentIndexChanged.connect(self.config_dropdown_changed)
-        
-        # Configuration layout
+
+    def configure_layouts(self):
+        """Configures the layouts for the UI."""
+        configuration_layout = self.create_configuration_layout()
+        control_buttons_layout = self.create_control_buttons_layout()
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.display_stack)
+        main_layout.addLayout(configuration_layout)
+        main_layout.addLayout(control_buttons_layout)
+        container = QWidget()
+        container.setLayout(main_layout)
+        self.setCentralWidget(container)
+
+    def create_configuration_layout(self):
+        """Creates the layout for the configuration widgets."""
         configuration_layout = QHBoxLayout()
         configuration_layout.addWidget(QLabel("IP address:"))
         configuration_layout.addWidget(self.ip_edit)
@@ -63,32 +94,22 @@ class MainWindow(QMainWindow):
         configuration_layout.addWidget(self.port_edit)
         configuration_layout.addWidget(QLabel("Config:"))
         configuration_layout.addWidget(self.config_dropdown)
-        
-        
-        # Control Buttons layout
+        return configuration_layout
+
+    def create_control_buttons_layout(self):
+        """Creates the layout for the control buttons."""
         control_buttons_layout = QHBoxLayout()
         control_buttons_layout.addWidget(self.button_start)
-        control_buttons_layout.addWidget(self.button_pause)
+        control_buttons_layout.addWidget(self.button_pause)       
         control_buttons_layout.addWidget(self.button_stop)
-        
-        # Main Layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.text_edit)
-        layout.addLayout(configuration_layout)
-        layout.addLayout(control_buttons_layout)
-        
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container) 
-        
-        # On startup not yet connected
-        self.connected = False
-    
+        return control_buttons_layout
+
     def config_dropdown_changed(self):
         if self.config_dropdown.currentText() == "main.py":
+            self.display_stack.setCurrentWidget(self.text_display)
             self.current_config = "main.py"
-            
         elif self.config_dropdown.currentText() == "cross.py":
+            self.display_stack.setCurrentWidget(self.table_display)
             self.current_config = "cross.py"
         
     def start(self):
@@ -123,7 +144,7 @@ class MainWindow(QMainWindow):
     
     def start_script(self, config):
         if config == "main.py":
-            asyncio.run(self.main_script())
+            asyncio.ensure_future(self.main_script())
         else: 
             self.log_and_print("Not implemented yet")
         
@@ -163,12 +184,14 @@ class MainWindow(QMainWindow):
                     self.log_and_print(f"{anchor_id}:{anchors[idx][1]}", end="")
                 self.log_and_print("]")
 
+    # TODO update to conform with cross script assumes rn that currentWidget is the text widget
     def log_and_print(self, message, end='\n'):
         """Log the message and print it to the console."""
-        self.log_and_print(self.ip_edit.text())
-        self.text_edit.insertPlainText(message + end)
-        if self.running:
-                self.text_edit.moveCursor(QTextCursor.End)
+        if self.config_dropdown.currentText() == "main.py":
+            print(message, end=end)
+            self.display_stack.currentWidget().insertPlainText(message + end)
+            if self.running:
+                    self.display_stack.currentWidget().moveCursor(QTextCursor.End)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
